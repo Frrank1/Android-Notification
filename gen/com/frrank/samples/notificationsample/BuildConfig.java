83e42d9e1a261bcaf26eceed553654e7d54beba7
Frrank <avveustudios@yahoo.com>
2016-05-01 08:05:10 -0700

    c

--- gen/com/frrank/samples/notificationsample/BuildConfig.java
+++ gen/com/frrank/samples/notificationsample/BuildConfig.java
@@ -1,6 +1,6 @@
 /*___Generated_by_IDEA___*/
 
-package com.varma.samples.notificationsample;
+package com.frrank.samples.notificationsample;
 
 /* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
 public final class BuildConfig {
